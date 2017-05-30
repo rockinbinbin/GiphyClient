9b280f8ac97436515f38d789c01de22ca70af41f
Robin <robimeht@umich.edu>
2017-05-30 08:07:10 -0700

    Initial Commit

---
+++ GiphyClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiphyClient
+//
+//  Created by Robin Mehta on 5/30/17.
+//  Copyright © 2017 robin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
