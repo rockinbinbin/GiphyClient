829df0e101f443f87173145f853c9f95b58dc089
Robin <robimeht@umich.edu>
2017-05-31 02:40:05 -0700

    textview

--- GiphyClient/ExpandedGifViewController.swift
+++ GiphyClient/ExpandedGifViewController.swift
@@ -15,7 +15,7 @@ import CHTCollectionViewWaterfallLayout
 
 // Use this view to type Gif caption and post to story
 
-class ExpandedGifViewController: UIViewController {
+class ExpandedGifViewController: UIViewController, UIScrollViewDelegate, UITextViewDelegate {
 
     public init(gif: Gif) {
         self.gif = gif
@@ -28,27 +28,53 @@ class ExpandedGifViewController: UIViewController {
 
     var gif : Gif
 
+    private lazy var scrollView: UIScrollView = {
+        let scrollView: UIScrollView = UIScrollView(frame: self.view.frame)
+        scrollView.bounces = false
+        return scrollView
+    }()
+
+    private lazy var textView: UITextView = {
+        let textView: UITextView = UITextView()
+        textView.textColor = UIColor.black
+        textView.backgroundColor = UIColor.white
+        self.scrollView.addSubview(textView)
+        return textView
+    }()
+
     private lazy var gifView: FLAnimatedImageView = {
         let gifView = FLAnimatedImageView()
         gifView.backgroundColor = UIColor.darkGray
         gifView.contentMode = .scaleAspectFill
-        self.view.addSubview(gifView)
+        self.scrollView.addSubview(gifView)
         return gifView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "SunriseGradient")!)
+        self.view.addSubview(scrollView)
+        scrollView.autoPinEdgesToSuperviewEdges()
         self.navigationController?.navigationBar.styleNavBar()
 
         gifView.autoSetDimension(.width, toSize: self.view.frame.size.width)
         gifView.autoPinEdge(toSuperviewEdge: .top, withInset: 0)
         gifView.autoAlignAxis(toSuperviewAxis: .vertical)
         self.loadGif(gif: self.gif, gifSize: .original)
+
+        textView.autoPinEdge(.top, to: .bottom, of: gifView, withOffset: 0)
+        textView.autoSetDimension(.width, toSize: self.view.frame.size.width)
+        textView.autoSetDimension(.height, toSize: 150)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let size = gif.getSize(gifSize: .original)
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: size.height + 200)
     }
 
     func loadGif(gif: Gif, gifSize: GifSize) {
